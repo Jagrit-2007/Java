6de7653b4e326b04b2c6b9061a27e91d60e9cbf3
Jagrit-2007 <jagrit.bansal0@gmail.com>
2026-06-30 12:59:51 +0530

    adding java folders

---
+++ myself/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args){
+
+
+
+        System.out.print("I like pizza\n");
+        System.out.println("Hello world");
+        System.out.println("println makes itself to a newline");
+        System.out.print("hello");
+
+        // writing sout and hit tab gives System.out.print() , so you dont have to type it
+
+        // THIS IS A COMMENT AND DOES NOT AFFECT THE CODE   
+
+        /*
+
+        This is a Multiline Comment
+
+        */
+
+    }
+}
