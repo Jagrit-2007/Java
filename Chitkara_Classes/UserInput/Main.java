34306682cbc0d36bc4a8afc2141320b038b397d9
Jagrit-2007 <jagrit.bansal0@gmail.com>
2026-07-03 18:47:06 +0530

    uploading how do we take user input

---
+++ Chitkara_Classes/UserInput/Main.java
@@ -0,0 +1,39 @@
+package Chitkara_Classes.UserInput;
+import java.util.Scanner; // for taking user input we always use Scanner class and we must import it
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+        String name;
+        int age;
+        boolean isStudent;
+
+        Scanner userInput = new Scanner(System.in); // Scanner is a class , then userInput is the variable 
+        // new alloactes new memory to Scanner class , 
+        // Scanner on right hand side converts the raw data from System.in
+
+        // NAME
+        System.out.print("Enter your name: ");
+        name = userInput.nextLine();
+        System.out.println("Hello " + name);
+
+
+        // AGE
+        System.out.print("Enter your age: ");
+        age = userInput.nextInt();
+        System.out.println("You are " + age + " years old");
+        userInput.nextLine(); // will clear buffer if any
+
+
+        // isStudent
+        System.out.print("Are you a student: (true or false): ");
+        isStudent = userInput.nextBoolean();
+        System.out.println("You are student: " + isStudent);
+        userInput.nextLine(); // will clear buffer if any
+
+
+        userInput.close(); // close the Scanner class
+    }
+}
