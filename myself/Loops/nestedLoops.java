35c95cb5f98068b501e018597616b99088016b57
Jagrit-2007 <jagrit.bansal0@gmail.com>
2026-07-14 12:10:30 +0530

    updating this file

--- myself/Loops/nestedLoops.java
+++ myself/Loops/nestedLoops.java
@@ -41,7 +41,7 @@ public class nestedLoops {
         // dynamic nested loops
 
         for(int i = 5; i >= 1; i--){
-            for(int j = 1; j <= i ; j++){
+            for(int j = 1; j <= i ; j++){  // moving from down to upwards
                 System.out.print(j + " ");
             }
             System.out.println();
