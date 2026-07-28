86d1e8dd16c973a75c40e0e141a87161144f8da7
Jagrit-2007 <jagrit.bansal0@gmail.com>
2026-07-28 23:17:04 +0530

    intro to oops

---
+++ myself/OOPS/Main.java
@@ -0,0 +1,16 @@
+package OOPS;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Object : an entity that holds data and can perform actions , it's a reference data type
+        // objects are stored in heap memory
+
+        Scanner sc = new Scanner(System.in); // here "sc" is object
+        // we create an object by "new" keyword, it allocates memory in heap
+
+        sc.close();
+
+    }
+}
