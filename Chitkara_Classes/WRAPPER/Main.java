a00382bdedf20c0984b06279cb92c1a3b6c6a9f3
Jagrit-2007 <jagrit.bansal0@gmail.com>
2026-07-29 18:56:21 +0530

    intro to wrapper class

---
+++ Chitkara_Classes/WRAPPER/Main.java
@@ -0,0 +1,33 @@
+package WRAPPER;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // WRAPPER CLASS : EVERY PRIMITIVE DATATYPE HAVE IT'S CORRESPONDING CLASS , THOSE CLASS IS CALLED WRAPPER CLASS !
+
+        // byte a;
+        // short b;
+        // int i;
+        // long l;
+        // float f;
+        // double dou;
+        // char c;
+        // boolean b1;
+
+        // Byte a1;
+        // Short a2;
+        // Integer a3;
+        // Long b3;
+        // Float f1;
+        // Double d1;
+        // Character ch1;
+        // Boolean b4;
+
+        int aa = 100;
+        Integer a = Integer.valueOf(aa);
+
+        Integer b = 10000;
+        int i = b.intValue();
+        
+    }
+}
