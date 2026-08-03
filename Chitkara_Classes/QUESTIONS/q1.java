d962c66d9c50223728dad10254e0e541ef461cbf
Jagrit-2007 <jagrit.bansal0@gmail.com>
2026-08-03 22:56:46 +0530

    practice questions of array & constructors

---
+++ Chitkara_Classes/QUESTIONS/q1.java
@@ -0,0 +1,63 @@
+package Chitkara_Classes.QUESTIONS;
+
+// public class Array{
+// public static void main(String[] args) {
+// int arr[][]={
+// {1,2,3},
+// {4,5,6},
+// {7,8,9}
+// };
+// int n = arr[0].length;
+// for(int i=0;i<arr.length;i++){
+// for(int j=0;j<arr[i].length;j++){
+// if(i==j){
+// System.out.println("primary digonal ele:"+arr[i][j]);
+// }
+// }
+// }
+// for(int i=0;i<arr.length;i++){
+// for(int j=0;j<arr[i].length;j++){
+// if(i+j==n-1){
+// System.out.println("secondary digonal ele:"+arr[i][j]);
+// }
+// }}
+// }
+// }
+
+// class Book{
+// int bookId;
+// String title;
+// String author;
+// double price;
+// int quantity;
+// double totalValues;
+
+// Book(int bookId,String title , String author , double price ,int quantity){
+// this.bookId=bookId;
+// this.title=title;
+// this.author=author;
+// this.price=price;
+// this.quantity=quantity;
+// }
+// void calculateTotalValue(){
+// totalValues=price*quantity;
+// }
+// void display(){
+// System.out.println("book id:"+bookId);
+// System.out.println("title:"+title);
+// System.out.println("author:"+author);
+// }
+// }
+// public class Main{
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int bookId = sc.nextInt();
+// String title=sc.nextLine();
+// String author=sc.nextLine();
+// double price = sc.nextDouble();
+// int quantity = sc.nextInt();
+// Book book = new Book(bookId ,title ,author, price ,quantity);
+// book.calculateTotalValue();
+// book.display();
+// }
+// }
